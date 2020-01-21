ca5b019f323de5a7b5da3d1a7bd403a8e12dabc6
dodo721 <dodoworkspr@gmail.com>
2020-01-21 12:37:01 +0000

    Changed ReportGenerator structure and removed ReportFetcher.java

--- src/main/java/com/napier/set08103/group27/report/ReportGenerator.java
+++ src/main/java/com/napier/set08103/group27/report/ReportGenerator.java
@@ -2,6 +2,11 @@ package com.napier.set08103.group27.report;
 
 public interface ReportGenerator {
 
+    // TODO: Create DatabaseManager
+    //ReportGenerator (DatabaseManager db) {
+
+    //}
+
     String[] generate ();
 
 }
