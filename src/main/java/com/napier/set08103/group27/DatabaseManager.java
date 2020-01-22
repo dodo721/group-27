8fde116f573333abac16bd6ffbf0a7715adcaf55
Sam Laurenson <40398765@live.napier.ac.uk>
2020-01-22 12:03:28 +0000

    Creating database class called DatabaseManager which will be used to provide a connection for the database we will be using.

---
+++ src/main/java/com/napier/set08103/group27/DatabaseManager.java
@@ -0,0 +1,15 @@
+package com.napier.set08103.group27;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseManager {
+
+    //Probably should make this a singleton
+
+    //Connect to mongodb
+    MongoClient mongoClient = new MongoClient();
+
+    //Get database
+    MongoDatabase database = mongoClient.getDatabase("world.sql");
+}
