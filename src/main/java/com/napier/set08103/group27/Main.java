9419806475167b382ec24aa601b4636184acc477
Sam Laurenson <40398765@live.napier.ac.uk>
2020-01-23 17:10:18 +0000

    Updating Main to have connections to the MySQL database.

--- src/main/java/com/napier/set08103/group27/Main.java
+++ src/main/java/com/napier/set08103/group27/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main (String[] args) {
         System.out.println("test");
+
+        DatabaseManager db = new DatabaseManager();
+
+        //Connecting to the database
+        db.connect();
+
+        //TODO: put SQL statements/methods to get from the database here
+
+        //Closing the connection to database
+        db.disconnect();
     }
 
 }
