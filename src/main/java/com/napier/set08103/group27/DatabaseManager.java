4b5853fa95e29d9977f721c2b39fa2d70d554d1a
Sam Laurenson <40398765@live.napier.ac.uk>
2020-01-23 17:09:10 +0000

    Creating class to manage connecting to and disconnecting from the database.

--- src/main/java/com/napier/set08103/group27/DatabaseManager.java
+++ src/main/java/com/napier/set08103/group27/DatabaseManager.java
@@ -1,15 +1,62 @@
 package com.napier.set08103.group27;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseManager {
 
-    //Probably should make this a singleton
+    private Connection connection = null;
 
-    //Connect to mongodb
-    MongoClient mongoClient = new MongoClient("mongodb-server");
+    //Method to connect to the MySQL database
+    public void connect()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println("Can't load SQL driver");
+            System.exit(-1);
+        }
 
-    //Get database
-    MongoDatabase database = mongoClient.getDatabase("world.sql");
+        int retries = 5;
+        for(int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database");
+            try
+            {
+                Thread.sleep(9000);
+                connection = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "password");
+                System.out.println("Success");
+                break;
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println("Thread interruption");
+            }
+            catch (SQLException e)
+            {
+                System.out.println("Failed after " + i + " retries.");
+            }
+        }
+    }
+
+    //Procedure to disconnect from the MySQL database
+    public void disconnect()
+    {
+        if (connection != null)
+        {
+            try
+            {
+                connection.close();
+            }
+            catch (SQLException e)
+            {
+               System.out.println("Error closing database");
+            }
+        }
+    }
+    
 }
