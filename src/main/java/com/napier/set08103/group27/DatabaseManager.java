e7b39f00a0faf92a08889d3c71acefcf9a6a2630
Sam Laurenson <40398765@live.napier.ac.uk>
2020-01-25 21:43:52 +0000

    Adding comments and working on connection between program and database

--- src/main/java/com/napier/set08103/group27/DatabaseManager.java
+++ src/main/java/com/napier/set08103/group27/DatabaseManager.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//Class to manage the connection between the program and the database
 public class DatabaseManager {
 
     private Connection connection = null;
@@ -13,6 +14,7 @@ public class DatabaseManager {
     {
         try
         {
+            //Loading the SQL driver
             Class.forName("com.mysql.jdbc.Driver");
         }
         catch(ClassNotFoundException e)
@@ -21,14 +23,17 @@ public class DatabaseManager {
             System.exit(-1);
         }
 
-        int retries = 5;
-        for(int i = 0; i < retries; ++i)
+        int retries = 5;    //Number of times connection will be attempted before providing an error
+        for(int i = 1; i <= retries; i++)
         {
             System.out.println("Connecting to database");
             try
             {
-                Thread.sleep(9000);
-                connection = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "password");
+                //Giving time for database to get set up
+                Thread.sleep(5000);
+
+                //Getting connection to the database
+                connection = DriverManager.getConnection("jdbc:mysql://db:3306/world", "root", "password");
                 System.out.println("Success");
                 break;
             }
@@ -43,9 +48,10 @@ public class DatabaseManager {
         }
     }
 
-    //Procedure to disconnect from the MySQL database
+    //Method to disconnect from the MySQL database
     public void disconnect()
     {
+        //If there is a connection then try and close the connection
         if (connection != null)
         {
             try
--- src/main/java/com/napier/set08103/group27/Main.java
+++ src/main/java/com/napier/set08103/group27/Main.java
@@ -3,12 +3,12 @@ package com.napier.set08103.group27;
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("test");
 
         DatabaseManager db = new DatabaseManager();
 
         //Connecting to the database
-        db.connect();
+        //Encountering problems with database connection
+        //db.connect();
 
         //TODO: put SQL statements/methods to get from the database here
 
