da6e981e5f846bdd691cd7de82ac73c7839ce8ff
Sam Laurenson <40398765@live.napier.ac.uk>
2020-03-14 15:07:52 +0000

    Working on creating integration tests

---
+++ src/unit-tests/java/com/napier/set08103/group27/AppIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.napier.set08103.group27;
+
+import com.napier.set08103.group27.data.City;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest {
+    static DatabaseManager databaseManager;
+    static DataStore dataStore = DataStore.getInstance();
+
+    @BeforeAll
+    static void init()
+    {
+        databaseManager = new DatabaseManager();
+        databaseManager.connect("http://192.168.99.100:33060");
+    }
+
+    @Test
+    void testGetCity()
+    {
+        databaseManager.readCities();
+        City city = dataStore.getCityHashMap().get(3);
+        assertEquals(city.getId(), 3);
+        assertEquals(city.getName(), "Herat");
+        assertEquals(city.getCountryCode(), "AFG");
+        assertEquals(city.getDistrict(), "Herat");
+        assertEquals(city.getPopulation(), 186800);
+    }
+}
