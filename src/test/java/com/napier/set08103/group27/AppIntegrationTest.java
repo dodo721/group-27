4c8b425e618912bf65b032ead3c53d8578e7fcff
Sam Laurenson <40398765@live.napier.ac.uk>
2020-03-15 11:24:03 +0000

    Created tests for app integration which can be used once local database connection is figured out

--- src/test/java/com/napier/set08103/group27/AppIntegrationTest.java
+++ src/test/java/com/napier/set08103/group27/AppIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.napier.set08103.group27;
 
 import com.napier.set08103.group27.data.City;
+import com.napier.set08103.group27.data.Country;
+import com.napier.set08103.group27.data.Languages;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +18,7 @@ public class AppIntegrationTest {
     static void init()
     {
         databaseManager = new DatabaseManager();
-        databaseManager.connect("db:33060");
+        databaseManager.connect("localhost:33060");
     }
 
     @Test
@@ -22,10 +26,59 @@ public class AppIntegrationTest {
     {
         databaseManager.readCities();
         City city = dataStore.getCityHashMap().get(3);
-        assertEquals(city.getId(), 3);
-        assertEquals(city.getName(), "Herat");
-        assertEquals(city.getCountryCode(), "AFG");
-        assertEquals(city.getDistrict(), "Herat");
-        assertEquals(city.getPopulation(), 186800);
+        assertEquals(3, city.getId());
+        assertEquals("Herat", city.getName());
+        assertEquals("AFG", city.getCountryCode());
+        assertEquals("Herat", city.getDistrict());
+        assertEquals(186800, city.getPopulation());
+    }
+
+    @Test
+    void testGetCountry()
+    {
+        databaseManager.readCountries();
+        Country country = dataStore.getCountriesHashMap().get("AGO");
+        assertEquals("AFG", country.getCode());
+        assertEquals("Afghanistan", country.getName());
+        assertEquals("Asia", country.getContinent());
+        assertEquals("Southern and Central Asia", country.getRegion());
+        assertEquals(652090.00, country.getSurfaceArea());
+        assertEquals(1919, country.getIndepYear());
+        assertEquals(22720000, country.getPopulation());
+        assertEquals(45.9, country.getLifeExpectancy());
+        assertEquals(5976.00, country.getGNP());
+        assertEquals(null, country.getGNPOld());
+        assertEquals("Afganistan/Afqanestan", country.getLocalName());
+        assertEquals("Islamic Emirate", country.getGovernmentForm());
+        assertEquals("Mohammad Omar", country.getHeadOfState());
+        assertEquals(1, country.getCapital());
+        assertEquals("AF", country.getCode2());
+    }
+
+    @Test
+    void testGetLanguages()
+    {
+        databaseManager.readLanguages();
+        ArrayList<Languages> languages = dataStore.getLanguageHashMap().get("ABW");
+
+        assertEquals("ABW", languages.get(0).getCountryCode());
+        assertEquals("Dutch", languages.get(0).getLanguage());
+        assertEquals(true, languages.get(0).isOfficial());
+        assertEquals(5.3, languages.get(0).getPercentage());
+
+        assertEquals("ABW", languages.get(1).getCountryCode());
+        assertEquals("English", languages.get(1).getLanguage());
+        assertEquals(false, languages.get(1).isOfficial());
+        assertEquals(9.5, languages.get(1).getPercentage());
+
+        assertEquals("ABW", languages.get(2).getCountryCode());
+        assertEquals("Papiamento", languages.get(2).getLanguage());
+        assertEquals(false, languages.get(2).isOfficial());
+        assertEquals(76.7, languages.get(2).getPercentage());
+
+        assertEquals("ABW", languages.get(3).getCountryCode());
+        assertEquals("Spanish", languages.get(3).getLanguage());
+        assertEquals(false, languages.get(3).isOfficial());
+        assertEquals(7.4, languages.get(3).getPercentage());
     }
 }
