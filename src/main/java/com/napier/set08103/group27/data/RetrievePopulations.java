137c946af848dfdbe742eb9a00034998d58833ca
Sam Laurenson <40398765@live.napier.ac.uk>
2020-03-18 18:51:11 +0000

    Adding methods for getting populations of region, country, district and city.

--- src/main/java/com/napier/set08103/group27/data/RetrievePopulations.java
+++ src/main/java/com/napier/set08103/group27/data/RetrievePopulations.java
@@ -32,4 +32,64 @@ public class RetrievePopulations {
 
         return continentPopulation;
     }
+
+    public long getRegionPopulation(String region)
+    {
+        long regionPopulation = 0;
+
+        for(String key : dataStore.getCountriesHashMap().keySet())
+        {
+            if(region.equals(dataStore.getCountriesHashMap().get(key).getRegion()))
+            {
+                regionPopulation += dataStore.getCountriesHashMap().get(key).getPopulation();
+            }
+        }
+
+        return regionPopulation;
+    }
+
+    public long getCountryPopulation(String country)
+    {
+        long countryPopulation = 0;
+
+        for(String key : dataStore.getCountriesHashMap().keySet())
+        {
+            if(country.equals(dataStore.getCountriesHashMap().get(key).getCode()))
+            {
+                countryPopulation += dataStore.getCountriesHashMap().get(key).getPopulation();
+            }
+        }
+
+        return countryPopulation;
+    }
+
+    public long getDistrictPopulation(String district)
+    {
+        long districtPopulation = 0;
+
+        for(int key : dataStore.getCityHashMap().keySet())
+        {
+            if(district.equals(dataStore.getCityHashMap().get(key).getDistrict()))
+            {
+                districtPopulation += dataStore.getCityHashMap().get(key).getPopulation();
+            }
+        }
+
+        return districtPopulation;
+    }
+
+    public long getCityPopulation(String city)
+    {
+        long cityPopulation = 0;
+
+        for(int key : dataStore.getCityHashMap().keySet())
+        {
+            if(city.equals(dataStore.getCityHashMap().get(key).getName()))
+            {
+                cityPopulation += dataStore.getCityHashMap().get(key).getPopulation();
+            }
+        }
+
+        return cityPopulation;
+    }
 }
