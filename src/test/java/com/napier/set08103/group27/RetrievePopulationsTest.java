951d9e3a573ef10a773e505142110b02cccccfdd
Sam Laurenson <40398765@live.napier.ac.uk>
2020-03-18 19:33:45 +0000

    Adding unit tests for RetrievePopulations

--- src/test/java/com/napier/set08103/group27/RetrievePopulationsTest.java
+++ src/test/java/com/napier/set08103/group27/RetrievePopulationsTest.java
@@ -1,5 +1,6 @@
 package com.napier.set08103.group27;
 
+import com.napier.set08103.group27.data.City;
 import com.napier.set08103.group27.data.Country;
 import com.napier.set08103.group27.data.RetrievePopulations;
 import org.junit.jupiter.api.BeforeAll;
@@ -66,6 +67,100 @@ public class RetrievePopulationsTest {
         System.out.println("Europe population: " + continentPop);
 
         assertEquals(999999999L, continentPop);
+        dataStore.clear();
+    }
+
+    @Test
+    void testRegionPopulation()
+    {
+        String code = "ABC";
+        String name = "TestCountry";
+        String continent = "Europe";
+        String region = "TestRegion";
+        double surfaceArea = 5.55;
+        int indepYear = 1990;
+        int population = 50505050;
+        double lifeExpectancy = 60.5;
+        double gnp = 5.5;
+        double gnpOld = 4.5;
+        String localName = "counTree";
+        String governmentForm = "Who";
+        String headOfState = "me";
+        int capital = 5;
+        String code2 = "CBA";
+        Country country = new Country(code, name, continent, region, surfaceArea, indepYear, population, lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, capital, code2);
+        dataStore.addToCountry(country);
+
+        long regionPop = retrievePopulations.getRegionPopulation("TestRegion");
+        System.out.println("TestRegion population: " + regionPop);
+
+        assertEquals(50505050L, regionPop);
+        dataStore.clear();
+    }
+
+    @Test
+    void testCountryPopulation()
+    {
+        String code = "ABC";
+        String name = "TestCountry";
+        String continent = "Europe";
+        String region = "TestRegion";
+        double surfaceArea = 5.55;
+        int indepYear = 1990;
+        int population = 50505050;
+        double lifeExpectancy = 60.5;
+        double gnp = 5.5;
+        double gnpOld = 4.5;
+        String localName = "counTree";
+        String governmentForm = "Who";
+        String headOfState = "me";
+        int capital = 5;
+        String code2 = "CBA";
+        Country country = new Country(code, name, continent, region, surfaceArea, indepYear, population, lifeExpectancy, gnp, gnpOld, localName, governmentForm, headOfState, capital, code2);
+        dataStore.addToCountry(country);
+
+        long countryPop = retrievePopulations.getCountryPopulation("ABC");
+        System.out.println("TestRegion population: " + countryPop);
+
+        assertEquals(50505050L, countryPop);
+        dataStore.clear();
+    }
+
+    @Test
+    void testDistrictPopulation()
+    {
+        int id = 5;
+        String name = "Name";
+        String countryCode = "AAA";
+        String district = "district";
+        int population = 10000;
+
+        City city = new City(id, name, countryCode, district, population);
+        dataStore.addToCity(city);
+
+        long districtPop = retrievePopulations.getDistrictPopulation("district");
+        System.out.println("District population: " + districtPop);
+
+        assertEquals(10000, districtPop);
+        dataStore.clear();
+    }
+
+    @Test
+    void getCityPopulation()
+    {
+        int id = 5;
+        String name = "Name";
+        String countryCode = "AAA";
+        String district = "district";
+        int population = 5050;
+
+        City city = new City(id, name, countryCode, district, population);
+        dataStore.addToCity(city);
+
+        long cityPop = retrievePopulations.getCityPopulation("Name");
+        System.out.println("City population: " + cityPop);
+
+        assertEquals(5050, cityPop);
         dataStore.clear();
     }
 }
