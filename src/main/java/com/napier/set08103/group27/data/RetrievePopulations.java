b29eede4caf7ebb2229a572d7b6440994897af55
Sam Laurenson <40398765@live.napier.ac.uk>
2020-03-18 19:33:59 +0000

    Adding comments to methods

--- src/main/java/com/napier/set08103/group27/data/RetrievePopulations.java
+++ src/main/java/com/napier/set08103/group27/data/RetrievePopulations.java
@@ -6,6 +6,7 @@ public class RetrievePopulations {
 
     DataStore dataStore = DataStore.getInstance();
 
+    //Method to add populations of all countries together to get world population
     public long getWorldPopulation()
     {
         long worldPopulation = 0;
@@ -18,6 +19,8 @@ public class RetrievePopulations {
         return worldPopulation;
     }
 
+    //Method to add all country populations in a continent together to get continent population
+    //Takes in a string which will be the continent
     public long getContinentPopulation(String continent)
     {
         long continentPopulation = 0;
@@ -33,6 +36,8 @@ public class RetrievePopulations {
         return continentPopulation;
     }
 
+    //Method to add all region populations in a region together to get region population
+    //Takes in string which will be region name
     public long getRegionPopulation(String region)
     {
         long regionPopulation = 0;
@@ -48,6 +53,8 @@ public class RetrievePopulations {
         return regionPopulation;
     }
 
+    //Method to get population of country
+    //Takes in country code
     public long getCountryPopulation(String country)
     {
         long countryPopulation = 0;
@@ -63,6 +70,8 @@ public class RetrievePopulations {
         return countryPopulation;
     }
 
+    //Method to get population in a district
+    //Takes in district name
     public long getDistrictPopulation(String district)
     {
         long districtPopulation = 0;
@@ -78,6 +87,8 @@ public class RetrievePopulations {
         return districtPopulation;
     }
 
+    //Method to get population in a city
+    //Takes in city name
     public long getCityPopulation(String city)
     {
         long cityPopulation = 0;
