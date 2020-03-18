de1d424e25458fd2cfb1539544c3d7bf3b7bf8b3
Sam Laurenson <40398765@live.napier.ac.uk>
2020-03-18 18:40:50 +0000

    Adding code to get reports for each continent (Debug).

--- src/main/java/com/napier/set08103/group27/Main.java
+++ src/main/java/com/napier/set08103/group27/Main.java
@@ -6,9 +6,11 @@ import com.napier.set08103.group27.report.ReportGenerator;
 import com.napier.set08103.group27.report.WorldPopulationReport;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Scanner scan = new Scanner(System.in);
     private static DataStore dataStore = DataStore.getInstance();
     private static ArrayList<ReportGenerator> reports = new ArrayList<ReportGenerator>();
 
@@ -47,8 +49,18 @@ public class Main {
 
         //Adding report containing number of speakers of a language in the world
         reports.add(new LanguagesOfWorldReport());
+
+        //Report to show world population
         reports.add(new WorldPopulationReport());
+
+        //Adding reports to get population for continents
         reports.add(new ContinentPopulationReport("Europe"));
+        reports.add(new ContinentPopulationReport("Asia"));
+        reports.add(new ContinentPopulationReport("North America"));
+        reports.add(new ContinentPopulationReport("Africa"));
+        reports.add(new ContinentPopulationReport("South America"));
+        reports.add(new ContinentPopulationReport("Oceania"));
+        reports.add(new ContinentPopulationReport("Antarctica"));
 
         //printing reports
         for(ReportGenerator report : reports)
